c0056037a0ac78ab9e83dc51be7207dc5e3132a4
domdom <dominic.kekkonen@gmail.com>
2026-02-18 12:49:46 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace konstruktsionid_ülesanded
+{
+        public class StartPage
+        {
+            public static void Main(string[] args)
+            {
+                Console.WriteLine("Tere, mis on sinu nimi?: ");
+                string eesnimi = "";
+                eesnimi = Console.ReadLine();
+                do
+                {
+                    Console.WriteLine("Mis on sinu vanus?: ");
+                    int vanus = 0;
+                    vanus = int.Parse(Console.ReadLine());
+                    if (vanus >= 6)
+                    {
+                        Console.WriteLine("piletid on tasuta");
+                    }
+                    else if (vanus <= 7 && vanus >= 14)
+                    {
+                        Console.WriteLine("see on lastepilet");
+                    }
+                    else if (vanus <= 15 && vanus >= 65)
+                    {
+                        Console.WriteLine("see on täispilet");
+                    }
+                    else if (vanus <= 66)
+                    {
+                        Console.WriteLine("sul on soosduspilet");
+                    }
+                    else if (vanus <= 0 && vanus >= 100)
+                    {
+                        Console.WriteLine("Viga");
+                    }
+                } while (eesnimi == "jukku");
+
+            }
+        }
+}

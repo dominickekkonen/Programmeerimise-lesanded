3c76112444c1bfcd0499d5a6a23bd04832e90a75
domdom <dominic.kekkonen@gmail.com>
2026-03-05 13:53:54 +0200

    Add files via upload

---
+++ Praktilised_funktsioonid.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Osa._4_Praktilised_ülesannded
+{
+    public static class Praktilised_funktsioonid
+    {
+        public static void LaeKoostisosad()
+        {
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Koostisosad.txt");
+            List<string> koostisosad = new List<string>();
+
+            try
+            {
+                string[] read = File.ReadAllLines(failitee);
+                koostisosad = new List<string>(read);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+        public static void salvestamine_faili()
+        {
+            Console.Write("Sisesta üks Itaalia toit: ");
+            string toit = Console.ReadLine();
+
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Retseptid.txt");
+
+            try
+            {
+                StreamWriter writer = new StreamWriter(path, true);
+                writer.WriteLine(toit);
+                writer.Close();
+
+                Console.WriteLine("Toit salvestati edukalt faili!");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Faili kirjutamisel tekkis viga");
+            }
+
+        }
+        public static void Koostisosade_muutmine()
+        {
+            string path = "Koostisosad.txt";
+
+            List<string> koostisosad = new List<string>(File.ReadAllLines(path));
+
+            koostisosad[0] = "Kvaliteetne oliiviõli";
+
+            koostisosad.Remove("Ketšup");
+
+            Console.WriteLine("Uuendatud koostisosade nimekiri:");
+
+            foreach (string i in koostisosad)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public static void Külmkappi_kontroll(List<string> koostisosad)
+        {
+            Console.Write("\nSisesta koostisosa, mida otsida: ");
+            string otsitav = Console.ReadLine();
+
+            if (koostisosad.Contains(otsitav))
+            {
+                Console.WriteLine("Koostisosa on olemas!");
+            }
+            else
+            {
+                Console.WriteLine("Seda koostisosa meil retseptis ei ole.");
+            }
+            File.WriteAllLines("Koostisosad.txt", koostisosad);
+
+            Console.WriteLine("Uus retsept on edukalt faili salvestatud!");
+        }
+
+    }
+}
---
+++ start_page.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+namespace Osa._4_Praktilised_ülesannded
+{
+    internal class start_page
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Vali tegevus: \n0 - Välju\n1 - Koostisosade muutmine\n2 - Salvestamine faili\n3 - Külmkappi kontroll\n");
+                int valik = int.Parse(Console.ReadLine());
+                switch (valik)
+                {
+                    default:
+                        break;
+                    case 0:
+                        Console.Clear(); ;
+                        return;
+                    case 1:
+                        Praktilised_funktsioonid.Koostisosade_muutmine();
+                        break;
+                    case 2:
+                        Praktilised_funktsioonid.salvestamine_faili();
+                        break;
+                    case 3:
+                        Praktilised_funktsioonid.Külmkappi_kontroll();
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+}
